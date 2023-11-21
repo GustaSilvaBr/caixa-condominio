59ef133a1bf5677e9cbf4bbe060eb49161920dd4
Guga <gustavo.ferreira0821@gmail.com>
2023-11-20 23:03:22 -0300

    :wrench: chore: get started with react-router-dom-6

---
+++ src/routes/root.tsx
@@ -0,0 +1,16 @@
+export function Root(){
+    return (
+        <div id="navigation-bar">
+            <nav>
+                <ul>
+                    <li>
+                        <a href="/flow">Flow</a>
+                    </li>
+                    <li>
+                        <a href="/apartments">apartments</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
